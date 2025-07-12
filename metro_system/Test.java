d3ef7bac9532e84f8c211c061c5bea6010ae0677
Gopikrishna Pinni <142527857+Gopikrishnapinni@users.noreply.github.com>
2025-07-12 10:35:35 +0530

    Update Test.java
    My First Change

--- metro_system/Test.java
+++ metro_system/Test.java
@@ -2,8 +2,8 @@
 public class Test {
 public static void main(String[] args) {
 		
-		System.out.println("hello there");
-			System.out.println("i think its time we should do our first commit");
+		System.out.println("hello");
+			System.out.println("I think its time we should do our first commit");
 
 	}
 }
